dbd9de81a7b83b6b4e33b7f9c183cefdc4b4d911
Henrik Frank <henrik@vonfrank.dk>
2016-07-14 00:41:26 +0200

    Created toolbar manually, and navigationview added. Different material design stuff added, mostly UI stuff..

--- app/src/main/java/dk/vonfrank/plexify/Model/Server.java
+++
@@ -1,7 +0,0 @@
-package dk.vonfrank.plexify.Model;
-
-/**
- * Created by Von Frank on 11-07-2016.
- */
-public class Server {
-}
---
+++ app/src/main/java/dk/vonfrank/plexify/View/MainActivity.java
@@ -0,0 +1,26 @@
+package dk.vonfrank.plexify.View;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+
+import dk.vonfrank.plexify.R;
+
+public class MainActivity extends Activity {
+
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+}
